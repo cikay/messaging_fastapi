3b14a0fa1ec107f4357fd46a2fcbe0abac6cea62
cikay <muzaffer123cikay@gmail.com>
2022-02-28 18:31:09 +0300

    Group sockets by conversation group id

--- websocket/connectionManager.py
+++ websocket/connectionManager.py
@@ -1,22 +1,32 @@
-from typing import List
+from typing import List, Dict
 
 from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.connection_groups: Dict[str, List[WebSocket]] = {}
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        id_ = self.get_conversationgroup_id(websocket)
+        connections = self.connection_groups.setdefault(id_, [])
+        if websocket not in connections:
+            connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        id_ = self.get_conversationgroup_id(websocket)
+        connections = self.connection_groups[id_]
+        connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
+    async def broadcast(self, message: str, sender_websocket):
+        id_ = self.get_conversationgroup_id(sender_websocket)
+        connections = self.connection_groups[id_]
+        for connection in connections:
             await connection.send_text(message)
 
+    @staticmethod
+    def get_conversationgroup_id(websocket):
+        return websocket.path_params['conversationgroup_id']
