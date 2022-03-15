accac3cfb19ec0cdb209fa0fd2b73aec02fa86f4
cikay <muzaffer123cikay@gmail.com>
2022-03-15 20:03:26 +0300

    Add login test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,6 @@
 
+from fastapi import status
 from fastapi.testclient import TestClient
-
 from main import app
 
 
@@ -16,3 +16,22 @@ def test_create_user():
     }
     response = client.post('users/create', json=data)
     assert response.status_code == 200
+
+def test_login():
+    data = {
+        'firstname': 'Test user',
+        'lastname': 'Test',
+        'username': 'test_user',
+        'password': '6g#-UKMrdZwF!Khq'
+    }
+    create_response = client.post('users/create', json=data)
+    assert create_response.status_code == 200
+
+    login_response = client.post('users/login', data={
+        'username': data['username'],
+        'password': data['password']
+    })
+
+    assert login_response.status_code == status.HTTP_200_OK
+    token_data = login_response.json()
+    assert 'access_token' in token_data 
