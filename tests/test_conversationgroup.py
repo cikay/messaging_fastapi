cc0b2a23ec109a3a709406a9165c100f05231842
cikay <muzaffer123cikay@gmail.com>
2022-03-19 01:30:18 +0300

    Break creating conversation group test

--- tests/test_conversationgroup.py
+++ tests/test_conversationgroup.py
@@ -30,7 +30,6 @@ def test_create_conversationgroup():
         'Authorization': f'Bearer {token_data["access_token"]}'
     }
     conversationgroup_payload = {
-        'name': 'Test',
         'users': []
     }
     cgroup_response = client.post(
